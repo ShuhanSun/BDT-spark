c4598b559265223880f821a56fc030f1013a6f0f
bookcool <sunshuhan666@163.com>
2019-09-20 09:56:02 -0500

    hive create table and select

--- SparkSqlProject/src/main/java/SparkSqlTest.java
+++ SparkSqlProject/src/main/java/SparkSqlTest.java
@@ -13,8 +13,7 @@ import java.util.Date;
 
 public class SparkSqlTest {
 
-    private static final String TWITTER_JSON_PATH = "src/main/resources/twitter.json";
-    private static final String TWITTER_JSON_PATH_100 = "src/main/resources/twitter100.json";
+    private static final String TWITTER_JSON_PATH_100 = "src/main/resources/twitter100.txt";
 
     /**
      * @param args
@@ -25,11 +24,11 @@ public class SparkSqlTest {
         SparkConf conf = new SparkConf().setAppName("twitter Application");
         conf.setMaster("local");
         JavaSparkContext sc = new JavaSparkContext(conf);
-        SQLContext sqlContext = new org.apache.spark.sql.SQLContext(sc);
+//        SQLContext sqlContext = new org.apache.spark.sql.SQLContext(sc);
 
-        DataFrame df = sqlContext.read().json(TWITTER_JSON_PATH_100);
-        df.show();
-        df.printSchema();
+//        DataFrame df = sqlContext.read().json(TWITTER_JSON_PATH_100);
+//        df.show();
+//        df.printSchema();
 
         // Select everybody, but increment the age by 1
         //        df.select(df.col("name"), df.col("age").plus(1)).show();
@@ -46,13 +45,30 @@ public class SparkSqlTest {
 //        all.count();
 
         // sc is an existing JavaSparkContext.
+        //TODO: 启动hive的元数据服务  $HIVE_HOME/conf/hive-site.xml,
+        //TODO: ${HIVE_HOME}/bin/hive --service metastore  1>/dev/null  2>&1  &
+        //TODO: 并将hive配置文件复制到spark conf下； $HIVE_HOME/lib/mysql-connector-java-5.1.12.jar copy或者软链到$SPARK_HOME/lib/
+        //Spark SQL自己也可创建元数据库，并不一定要依赖hive创建元数据库，所以不需要一定启动hive，
         HiveContext hsqlContext = new org.apache.spark.sql.hive.HiveContext(sc);
+        hsqlContext.setConf("hive.metastore.warehouse.dir", "/Users/sunshuhan/Documents/mum/course/BDT/BDT-spark/SparkSqlProject/spark-warehouse");
 
-        //hive table: created_at, id_str, text, user_id, user_name, user_location
-        hsqlContext.sql("CREATE TABLE IF NOT EXISTS twitter (id_str String, created_at DATE, text String, user String)");
-        hsqlContext.sql("LOAD DATA LOCAL INPATH '" + TWITTER_JSON_PATH_100 + "' INTO TABLE twitter");
+        //TODO: 提出去，每次都插入
+//        hsqlContext.sql("CREATE TABLE IF NOT EXISTS twitter " +
+//                "(id_str String, " +
+//                "created_at String, " +
+//                "user_id String, " +
+//                "user_name String, " +
+//                "user_location String, " +
+//                "text String) " +
+////                "PARTITIONED BY (id_str STRING) " +
+//                "ROW FORMAT DELIMITED FIELDS TERMINATED BY ';' LINES TERMINATED BY '\\n'");
+//        //CREATE EXTERNAL TABLE  ... LOCATION '/user/hive/cs523';
+//
+//        hsqlContext.sql("LOAD DATA LOCAL INPATH '" + TWITTER_JSON_PATH_100 + "' INTO TABLE twitter");
 
-        Row[] results = hsqlContext.sql("FROM twitter SELECT id_str, created_at, text").collect();
+        Row[] results = hsqlContext.sql("SELECT id_str, user_id, user_name, user_location, created_at, text FROM twitter ").collect();
+        System.out.println(results.length);
+
         for (Row row :
                 results) {
             System.out.println(row.toString());
@@ -63,10 +79,13 @@ public class SparkSqlTest {
 //        SimpleDateFormat dateFormat = new SimpleDateFormat("EEE MMM dd HH:mm:ss Z yyyy");
 //        String dateStart = "Thu Apr 06 15:24:15 +0000 2017";
 //        String dateEnd = "Thu Apr 08 15:24:15 +0000 2017";
-//        DataFrame d = sqlContext.sql("SELECT * FROM twitter WHERE created_at >= '" + dateStart + "' AND created_at >= '" + dateEnd+"'");
+//        Row[] results1 = hsqlContext.sql("SELECT id_str, created_at, text FROM twitter  WHERE created_at >= '" + dateStart + "' AND created_at <= '" + dateEnd + "'").collect();
+
 //        System.out.println(d.count());
 
         //2. How many average word count of twitter in different location?
+
+
         //3. What’s the count of max twitters of one user and what’s his/her name?
     }
 }
--- SparkSqlProject/src/main/java/Utils.java
+++ SparkSqlProject/src/main/java/Utils.java
@@ -1,25 +1,33 @@
 import bean.Twitter;
 import com.alibaba.fastjson.JSONArray;
 
+import java.io.*;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Objects;
 
 public class Utils {
     private static final SimpleDateFormat DATE_FORMAT = new SimpleDateFormat("EEE MMM dd HH:mm:ss Z yyyy");
 
     public static String twitterJson2String(String json) {
+        if (json == null) {
+            return null;
+        }
         Twitter twitter;
         try {
             twitter = JSONArray.parseObject(json, Twitter.class);
-        }catch (Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
             return null;
         }
 
-        if(twitter == null){
+        if (twitter == null) {
             return null;
         }
         StringBuilder sb = new StringBuilder();
+        if (twitter.getId_str() == null || twitter.getId_str().length() == 0) {
+            return null;
+        }
         sb.append(twitter.getId_str());
         sb.append(";");
         Date parse = null;
@@ -30,23 +38,42 @@ public class Utils {
             e.printStackTrace();
         }
         sb.append(";");
-        if(twitter.getUser() != null){
-            sb.append(twitter.getUser().getId());
-            sb.append(";");
-            sb.append(twitter.getUser().getName());
-            sb.append(";");
-            sb.append(twitter.getUser().getLocation());
-            sb.append(";");
+        if (twitter.getUser() == null) {
+            return null;
         }
-        if(twitter.getText() != null){
-            sb.append(twitter.getText().replace(";", "."));
-        }
+        sb.append(twitter.getUser().getId());
+        sb.append(";");
+        sb.append(twitter.getUser().getName());
+        sb.append(";");
+        sb.append(twitter.getUser().getLocation());
+        sb.append(";");
+        sb.append(twitter.getText().replaceAll("\n", " ").replaceAll(";", "."));
 
         return sb.toString();
     }
 
-    public static void main(String[] args) {
-        String json = "{ \"created_at\": \"Thu Apr 06 15:24:15 +0000 2017\", \"id_str\": \"850006245121695744\", \"text\": \"1\\/ Today we\\u2019re sharing our vision for the future of the Twitter API platform!\\thttps:\\/\\/t.co\\/XweGngmxlP\", \"user\": { \"id\": 2244994945, \"name\": \"Twitter Dev\", \"screen_name\": \"TwitterDev\", \"location\": \"Internet\", \"url\": \"https:\\/\\/dev.twitter.com\\/\", \"description\": \"Your official source for Twitter Platform news, updates & events. Need technical help? Visit https:\\/\\/twittercommunity.com\\/ \\u2328\\ufe0f #TapIntoTwitter\" }, \"place\": { }, \"entities\": { \"hashtags\": [ ], \"urls\": [ { \"url\": \"https:\\/\\/t.co\\/XweGngmxlP\", \"unwound\": { \"url\": \"https:\\/\\/cards.twitter.com\\/cards\\/18ce53wgo4h\\/3xo1c\", \"title\": \"Building the Future of the Twitter API Platform\" } } ], \"user_mentions\": [ ] } }";
-        System.out.println(twitterJson2String(json));
+    public static void main(String[] args) throws IOException {
+//        String json = "{ \"created_at\": \"Thu Apr 06 15:24:15 +0000 2017\", \"id_str\": \"850006245121695744\", \"text\": \"1\\/ Today we\\u2019re sharing our vision for the future of the Twitter API platform!\\thttps:\\/\\/t.co\\/XweGngmxlP\", \"user\": { \"id\": 2244994945, \"name\": \"Twitter Dev\", \"screen_name\": \"TwitterDev\", \"location\": \"Internet\", \"url\": \"https:\\/\\/dev.twitter.com\\/\", \"description\": \"Your official source for Twitter Platform news, updates & events. Need technical help? Visit https:\\/\\/twittercommunity.com\\/ \\u2328\\ufe0f #TapIntoTwitter\" }, \"place\": { }, \"entities\": { \"hashtags\": [ ], \"urls\": [ { \"url\": \"https:\\/\\/t.co\\/XweGngmxlP\", \"unwound\": { \"url\": \"https:\\/\\/cards.twitter.com\\/cards\\/18ce53wgo4h\\/3xo1c\", \"title\": \"Building the Future of the Twitter API Platform\" } } ], \"user_mentions\": [ ] } }";
+//        System.out.println(twitterJson2String(json));
+
+        String output = "/Users/sunshuhan/Documents/mum/course/BDT/BDT-spark/SparkSqlProject/src/main/resources/twitter100.txt";
+        FileInputStream inputStream = new FileInputStream("/Users/sunshuhan/Documents/mum/course/BDT/BDT-spark/SparkSqlProject/src/main/resources/twitter100.json");
+        //BufferedReader是可以按行读取文件
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+
+        FileWriter writer = new FileWriter(output, true);
+        String str = null;
+        while ((str = bufferedReader.readLine()) != null) {
+            String s = twitterJson2String(str);
+            if (s != null && s.length() != 0) {
+                writer.write(s + "\n");
+            }
+        }
+
+        //close
+        inputStream.close();
+        bufferedReader.close();
+        writer.close();
+
     }
 }
